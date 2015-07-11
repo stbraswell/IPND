564174e9922ba93796a58718b18e4c89836f6193
Troy Braswell <stbraswell@gmail.com>
2015-07-10 22:05:26 -0700

    4th commit
    1) updated code with new notes
    2) corrected variable in get_stage_id

--- Stage_2_Project-Python_Code.py
+++ Stage_2_Project-Python_Code.py
@@ -1,13 +1,6 @@
-#Troy Braswell
-#7/8/2015
-
-#This program takes in text (in a specific format) and returns the html code for my notes webpage using that text
-
-
-# generates the html code for a given section
 def generate_html(stage,title,description,notes):
     html_stage = '''
-<div id=''' + stage + '''>'''
+<div id="''' + stage + '''">'''
     
     html_title = '''
     <h3>''' + title + '''</h3>'''
@@ -26,29 +19,32 @@ def generate_html(stage,title,description,notes):
     full_html_text = html_stage + html_title + html_description + html_note + html_end
     return full_html_text
 
-# Gets the stage id of the concept
+# stage = "stage 1"
+# title = "title 1"
+# description = "description 1"
+# note = "note 1"
+    
+# print generate_html(stage,title,description,note)
+
 def get_stage_id(concept):
     start_location = concept.find('STAGE:')
     end_location = concept.find('TITLE:')
-    title = concept[start_location+7 : end_location-1]
-    return title
+    stage = concept[start_location+7 : end_location-1]
+    return stage
 
-# gets the title of the concept
 def get_title(concept):
     start_location = concept.find('TITLE:')
     end_location = concept.find('DESCRIPTION:')
     title = concept[start_location+7 : end_location-1]
     return title
 
-#gets the description of the concept
 def get_description(concept):
     start_location = concept.find('DESCRIPTION:')
     end_location = concept.find('NOTE:')
     description = concept[start_location+13 :end_location-1]
     return description
 
-#Stores, in a list, the positions of the text "NOTE: "
-def list_positions_in_string(text):                     
+def list_positions_in_string(text):                     # takes in text string and creates "list1" which contains the position of 'hello'
     i=0
     counter = 0
     list1 = []
@@ -59,9 +55,8 @@ def list_positions_in_string(text):
         i = number + 1
         counter += 1
     return list1
-
-#takes in text and output of list_positions_in_string, outputs the text of each position of text string
-def stringlist(text,strings):                           
+            
+def stringlist(text,strings):                           #takes in text and output of list_positions_in_string, outputs the text of each position of text string
     i = 0
     textlist=[]
     length = len(strings) - 1
@@ -72,7 +67,6 @@ def stringlist(text,strings):
         i += 1
     return textlist
 
-# creates the html code for the list items
 def get_notes(concept):
     position_of_notes = list_positions_in_string(concept)
     number_of_notes = len(position_of_notes) - 1
@@ -85,7 +79,6 @@ def get_notes(concept):
     note_code = note_code.rstrip('\n')
     return note_code
 
-#gets the text of a specific concept from the full note text
 def get_concept_by_number(text, concept_number):
     counter = 0
     while counter < concept_number:
@@ -100,29 +93,28 @@ def get_concept_by_number(text, concept_number):
         text = text[next_concept_end:]
     return concept
 
-TEST_TEXT = """STAGE: Stage 1
-TITLE: Why Computers are Stupid
-DESCRIPTION: The phrase "computers are stupid" refers 
-to how they interpret instructions literally. This 
-means that small typos can cause big problems.
-NOTE: note1
-NOTE: note2
-STAGE: Stage 2
-TITLE: Python
-DESCRIPTION: Python is a "programming language." It 
-provides programmers a way to write instructions for a 
-computer to execute in a way that the computer can understand.
-NOTE: note3
-NOTE: note4
-STAGE: Stage 3
-TITLE: While Loops
-DESCRIPTION: A while loop repeatedly executes the body of
-the loop until the "test condition" is no longer true.
-NOTE: note5
-NOTE: note6
+TEST_TEXT = """STAGE: stage-2-14
+TITLE: Structured Data: Lists
+DESCRIPTION: A list is a sequence of Anything - characters, strings, numbers...even other lists!
+NOTE: Lists take the form: <list> = [<expression>,<expression>,...]
+NOTE: Empty list => []
+NOTE: Elements of a list start from 0 so: [0,1,2,3...]
+NOTE: When defining a list, especially a long list, you can split the <expression>'s up *****example*****
+NOTE: You can even index within an indexed list. i.e. list1 = [['Troy','is'],['cool','!']- print list1[1][1] => will print the 4th element of the list, in this case '!'
+STAGE: stage-2-15
+TITLE: List Mutations and Aliasing
+DESCRIPTION: Lists support Mutations and Aliasing.  With mutation you are able to change the value of a list after it has been created.  With Aliasing, you can assign a list to two separate names; however if you mutate the list for one, you mutate it for both. 
+NOTE: Mutation example:
+NOTE: Aliasing example:
+STAGE: stage-2-16
+TITLE: List Operations
+DESCRIPTION: List operations allow you to perform certain tasks on lists
+NOTE: The "append" operation lets you insert another element into an existing list: <list>.append(<element>)
+NOTE: The "plus" operation is like concatination for lists: [1,2] + [3,4] => [1,2,3,4]
+Note: The "len" operation outputs the number of elements in a list (this also works on strings): len([0,1]) => 2
 """
 
-# Generates the complete html code for the input notes
+
 def generate_all_html(text):
     current_concept_number = 1
     concept = get_concept_by_number(text, current_concept_number)
@@ -132,6 +124,7 @@ def generate_all_html(text):
         title = get_title(concept)
         description = get_description(concept)
         notes = get_notes(concept)
+        #notes = "STAGE NOTE"
         concept_html = generate_html(stage,title,description,notes)
         all_html = all_html + concept_html
         current_concept_number = current_concept_number + 1
